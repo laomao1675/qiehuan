c0ccb21cb9d568958575bf3dc0731fb1e65c3414
laomao1675 <124508738+laomao1675@users.noreply.github.com>
2024-06-02 12:23:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function resetTimer() {
     clearTimeout(timer);
     timer = setTimeout(() => {
         window.location.href = "index.html"; // 返回到全屏图片切换页面
-    }, 60000); // 2分钟无操作
+    }, 120000); // 2分钟无操作
 }
 
 function setupActivityListeners() {
