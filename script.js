e7e46daa3c6216d8ad07f12ad9b1a311fe1d1443
laomao1675 <124508738+laomao1675@users.noreply.github.com>
2024-06-02 13:16:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,28 +11,3 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 5000); // 5秒切换
 }
-
-function goToStartPage() {
-    window.location.href = "start.html"; // 跳转到开始页面
-}
-
-// 处理用户活动检测和返回逻辑
-let timer;
-
-function resetTimer() {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-        window.location.href = "index.html"; // 返回到全屏图片切换页面
-    }, 120000); // 2分钟无操作
-}
-
-function setupActivityListeners() {
-    window.onload = resetTimer;
-    window.onmousemove = resetTimer;
-    window.onmousedown = resetTimer;
-    window.ontouchstart = resetTimer;
-    window.onclick = resetTimer;
-    window.onkeypress = resetTimer;
-}
-
-setupActivityListeners();
