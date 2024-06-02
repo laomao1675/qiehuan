0f47c89682f0c23e60cc4b020999c8a44503b3ca
laomao1675 <124508738+laomao1675@users.noreply.github.com>
2024-06-02 13:28:55 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+let timer;
+
+function resetTimer() {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        window.location.href = "index.html"; // 返回到全屏图片切换页面
+    }, 120000); // 2分钟无操作
+}
+
+function setupActivityListeners() {
+    window.onload = resetTimer;
+    window.onmousemove = resetTimer;
+    window.onmousedown = resetTimer;
+    window.ontouchstart = resetTimer;
+    window.onclick = resetTimer;
+    window.onkeypress = resetTimer;
+}
+
+setupActivityListeners();
